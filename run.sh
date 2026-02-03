ec669c7be18a91afa868a30bc3bab965d4d13f10
ManuFlosoYT <46373952+ManuFlosoYT@users.noreply.github.com>
2026-02-03 16:30:49 +0100

    run script

--- run.sh
+++ run.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 set -e
+
 if [ -z "$1" ]; then
     echo "Uso: ./run.sh <nombre_bin_sin_extension>"
     exit 1
 fi
 
-BIN_FILE="./Programas/build/$1.bin"
+mkdir -p ./Programas/build
 
-if [ ! -f "$BIN_FILE" ]; then
-    echo "Error: El archivo $BIN_FILE no existe, revisa que el binario este compilado (comp_bin.sh) y que el nombre sea correcto (sin extension)"
+SOURCE="./Programas/$1.c"
+if [ ! -f "$SOURCE" ]; then
+    echo "Error: El fichero $SOURCE no existe. Revisa que este en la carpeta Programas y que el nombre sea correcto (sin extension)"
     exit 1
 fi
 
+echo "--- Compilando $1 ---"
+cl65 -O --cpu 65C02 -t none -C Linker/memoria.cfg -o "./Programas/build/$1.bin" -m "./Programas/build/$1.map" -l "./Programas/build/$1.lst" Linker/inicio.s "$SOURCE"
+mv "./Programas/$1.o" "./Programas/build/$1.o"
+echo "Binario compilado correctamente"
+echo ""
+
+# Ejecutar el simulador
 ./build/SIM_65C02 "$1"
