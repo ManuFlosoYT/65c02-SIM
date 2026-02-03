b04cbee95d3133720133f56e5897be4d54dcd2e7
ManuFlosoYT <46373952+ManuFlosoYT@users.noreply.github.com>
2026-02-03 16:31:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 
 #include "Componentes/CPU.h"
-#include "Componentes/LCD.h"
 #include "Componentes/Mem.h"
+#include "Componentes/LCD.h"
 
 void CargarBinario(Mem& mem, const std::string& bin) {
     std::string ruta = "Programas/build/" + bin + ".bin";
