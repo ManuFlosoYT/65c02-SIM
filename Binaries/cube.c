ba1cc6f7d6ceea1db57370cb7c63017b9f2c09ed
ManuFlosoYT <46373952+ManuFlosoYT@users.noreply.github.com>
2026-02-18 11:32:52 +0100

    Optimize cube.c

--- Binaries/cube.c
+++ Binaries/cube.c
@@ -44,7 +44,7 @@ void via_init(void) {
 }
 
 int vx[8], vy[8], vz[8];
-int px[8], py[8];
+signed char px[8], py[8];
 
 #define C_SIZE (22 * FP_SCALE)
 
@@ -82,23 +82,29 @@ void rotate_all(unsigned char ax, unsigned char ay, unsigned char az) {
     int sin_z = ISIN(az), cos_z = ICOS(az);
 
     for (i = 0; i < 8; ++i) {
-        x = cvx[i]; y = cvy[i]; z = cvz[i];
+        x = cvx[i];
+        y = cvy[i];
+        z = cvz[i];
 
         /* X Rot */
         ty = (int)(((long)y * cos_x - (long)z * sin_x) >> FP_SHIFT);
         tz = (int)(((long)y * sin_x + (long)z * cos_x) >> FP_SHIFT);
-        y = ty; z = tz;
+        y = ty;
+        z = tz;
 
         /* Y Rot */
         tx = (int)(((long)x * cos_y + (long)z * sin_y) >> FP_SHIFT);
         tz = (int)(((long)-x * sin_y + (long)z * cos_y) >> FP_SHIFT);
-        x = tx; z = tz;
+        x = tx;
+        z = tz;
 
         /* Z Rot */
         tx = (int)(((long)x * cos_z - (long)y * sin_z) >> FP_SHIFT);
         ty = (int)(((long)x * sin_z + (long)y * cos_z) >> FP_SHIFT);
-        
-        vx[i] = tx; vy[i] = ty; vz[i] = z;
+
+        vx[i] = tx;
+        vy[i] = ty;
+        vz[i] = z;
     }
 }
 
@@ -114,11 +120,10 @@ void project_all(void) {
         if (z_shifted < 10) z_shifted = 10;
 
         factor_precalc = (long)vx[i] * factor;
-        px[i] = (int)((factor_precalc >> 8) / z_shifted) + (GPU_WIDTH / 2);
+        px[i] = (signed char)((int)((factor_precalc >> 8) / z_shifted) + (GPU_WIDTH / 2));
 
         factor_precalc = (long)vy[i] * factor;
-        py[i] = (int)((factor_precalc >> 8) / z_shifted);
-        py[i] = (GPU_HEIGHT / 2) - py[i];
+        py[i] = (signed char)((GPU_HEIGHT / 2) - (int)((factor_precalc >> 8) / z_shifted));
     }
 }
 
@@ -128,28 +133,34 @@ void init_graphics(void) {
     gpu_fill_screen(0x00);
 }
 
+const unsigned char black_color = 0x00;
+
 int main(void) {
     unsigned char ang_x, ang_y, ang_z;
     char speed_x, speed_y, speed_z;
-    register unsigned char i; 
+    register unsigned char i;
     unsigned char f, p1, p2, p3;
     unsigned char count;
     int v1x, v1y, v2x, v2y;
     long cross;
     RenderFace temp_face;
     int j_sort;
-    
+
+    /* Bounding box for clearing */
+    signed char min_x, max_x, min_y, max_y;
+    signed char clear_min_x = 0, clear_max_x = 0;
+    signed char clear_min_y = 0, clear_max_y = 0;
+
     unsigned int seed = 0;
 
     init_graphics();
     via_init();
-    
-    print_str("PRESS 2 KEYS TO START: ");
-    
+
+    print_str("PRESS ANY KEY TO GENERATE SEED: ");
+
     while (bios_getchar_nb() == 0) {
         seed++;
     }
-    bios_getchar(); 
 
     srand(seed);
 
@@ -170,7 +181,13 @@ int main(void) {
     if (rand() & 1) speed_z = -speed_z;
 
     while (1) {
-        gpu_fill_screen(0x00);
+        /* Clear previous frame using bounding box */
+        if (clear_max_x >= clear_min_x && clear_max_y >= clear_min_y) {
+            gpu_draw_rect(clear_min_x, clear_min_y,
+                          clear_max_x - clear_min_x + 1,
+                          clear_max_y - clear_min_y + 1,
+                          black_color);
+        }
 
         ang_x += speed_x;
         ang_y += speed_y;
@@ -178,6 +195,30 @@ int main(void) {
 
         rotate_all(ang_x, ang_y, ang_z);
         project_all();
+
+        /* Calculate new bounding box for next clear */
+        min_x = 127;
+        max_x = -128;
+        min_y = 127;
+        max_y = -128;
+
+        for (i = 0; i < 8; ++i) {
+            if (px[i] < min_x) min_x = px[i];
+            if (px[i] > max_x) max_x = px[i];
+            if (py[i] < min_y) min_y = py[i];
+            if (py[i] > max_y) max_y = py[i];
+        }
+
+        /* Clamp to screen area */
+        if (min_x < 0) min_x = 0;
+        if (max_x >= GPU_WIDTH) max_x = GPU_WIDTH - 1;
+        if (min_y < 0) min_y = 0;
+        if (max_y >= GPU_HEIGHT) max_y = GPU_HEIGHT - 1;
+
+        clear_min_x = min_x;
+        clear_max_x = max_x;
+        clear_min_y = min_y;
+        clear_max_y = max_y;
 
         count = 0;
         for (i = 0; i < 12; ++i) {
@@ -194,7 +235,7 @@ int main(void) {
 
             if (cross >= 0) {
                 visible_faces[count].id = i;
-                visible_faces[count].z_depth = vz[p1] + vz[p2] + vz[p3]; 
+                visible_faces[count].z_depth = vz[p1] + vz[p2] + vz[p3];
                 count++;
             }
         }
