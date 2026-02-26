102f32ab8571fbc3380b0316f83cca8d919a27e8
ManuFlosoYT <46373952+ManuFlosoYT@users.noreply.github.com>
2026-02-26 23:12:29 +0100

    Refactor BusDevice interface

--- Hardware/Core/IBusDevice.h
+++ Hardware/Core/IBusDevice.h
@@ -9,26 +9,29 @@ using Byte = uint8_t;
 using Word = uint16_t;
 
 class IBusDevice {
-public:
+   public:
+    IBusDevice() = default;
     virtual ~IBusDevice() = default;
+    IBusDevice(const IBusDevice&) = delete;
+    IBusDevice& operator=(const IBusDevice&) = delete;
+    IBusDevice(IBusDevice&&) = delete;
+    IBusDevice& operator=(IBusDevice&&) = delete;
 
     virtual Byte Read(Word address) = 0;
     virtual void Write(Word address, Byte data) = 0;
 
-    virtual std::string GetName() const = 0;
+    [[nodiscard]] virtual std::string GetName() const = 0;
 
     virtual void Reset();
 
     virtual bool SaveState(std::ostream& out) const = 0;
-    virtual bool LoadState(std::istream& in) = 0;
+    virtual bool LoadState(std::istream& inStream) = 0;
 
-    // Optional direct memory access for cache optimization (returns nullptr if
-    // not supported)
-    virtual Byte* GetRawMemory() { return nullptr; }
-    virtual bool IsReadOnly() const { return false; }
+    // Optional direct memory access for cache optimization (returns nullptr if not supported)
+    [[nodiscard]] virtual Byte* GetRawMemory() { return nullptr; }
+    [[nodiscard]] virtual bool IsReadOnly() const { return false; }
 };
 
 }  // namespace Hardware
-
 
 inline void Hardware::IBusDevice::Reset() {}
