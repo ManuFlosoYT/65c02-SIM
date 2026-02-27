848a4586a2642fed8fa6ed5157925aac2ec0c931
ManuFlosoYT <46373952+ManuFlosoYT@users.noreply.github.com>
2026-02-27 17:28:26 +0100

    Update DisassemblerWindow.cpp

--- Frontend/GUI/Debugger/DisassemblerWindow.cpp
+++ Frontend/GUI/Debugger/DisassemblerWindow.cpp
@@ -7,53 +7,48 @@
 namespace GUI {
 
 void DrawDisassemblerContent(Control::AppState& state) {
-    ImGui::BeginChild("DisassemblyScroll", ImVec2(0, 0), true);
+    ImGui::BeginChild("DisassemblyScroll", ImVec2(0, 0), ImGuiChildFlags_Borders);
 
     if (ImGui::BeginTable("DisassemblyTable", 4,
-                          ImGuiTableFlags_RowBg | ImGuiTableFlags_ScrollY |
-                              ImGuiTableFlags_BordersOuter |
+                          ImGuiTableFlags_RowBg | ImGuiTableFlags_ScrollY | ImGuiTableFlags_BordersOuter |
                               ImGuiTableFlags_BordersV)) {
-        ImGui::TableSetupColumn("Address", ImGuiTableColumnFlags_WidthFixed,
-                                80.0f);
-        ImGui::TableSetupColumn("Bytes", ImGuiTableColumnFlags_WidthFixed,
-                                80.0f);
-        ImGui::TableSetupColumn("Mnemonic", ImGuiTableColumnFlags_WidthFixed,
-                                80.0f);
+        ImGui::TableSetupColumn("Address", ImGuiTableColumnFlags_WidthFixed, 80.0F);
+        ImGui::TableSetupColumn("Bytes", ImGuiTableColumnFlags_WidthFixed, 80.0F);
+        ImGui::TableSetupColumn("Mnemonic", ImGuiTableColumnFlags_WidthFixed, 80.0F);
         ImGui::TableSetupColumn("Operands", ImGuiTableColumnFlags_WidthStretch);
         ImGui::TableHeadersRow();
 
-        uint16_t pc = state.emulator.GetCPU().PC;
+        uint16_t progCounter = state.emulator.GetCPU().PC;
         // Search back a few bytes to find a potential start point
         // This is heuristic, but for short ranges it usually works
-        uint16_t currentAddr = (pc > 32) ? pc - 32 : 0;
+        uint16_t currentAddr = (progCounter > 32) ? progCounter - 32 : 0;
 
         // Dissassemble a block around PC
         for (int i = 0; i < 50; i++) {
-            Hardware::DisassembledInstruction di =
-                Hardware::Disassembler::Disassemble(state.emulator.GetMem(),
-                                                    currentAddr);
+            Hardware::DisassembledInstruction instr =
+                Hardware::Disassembler::Disassemble(state.emulator.GetMem(), currentAddr);
 
             ImGui::TableNextRow();
-            if (currentAddr == pc) {
-                ImGui::TableSetBgColor(
-                    ImGuiTableBgTarget_RowBg0,
-                    ImGui::GetColorU32(ImVec4(0.3f, 0.3f, 0.7f, 0.6f)));
+            if (currentAddr == progCounter) {
+                ImGui::TableSetBgColor(ImGuiTableBgTarget_RowBg0, ImGui::GetColorU32(ImVec4(0.3F, 0.3F, 0.7F, 0.6F)));
             }
 
             ImGui::TableNextColumn();
-            ImGui::Text("%s", di.address.c_str());
+            ImGui::TextUnformatted(instr.address.c_str());
 
             ImGui::TableNextColumn();
-            ImGui::Text("%s", di.bytes.c_str());
+            ImGui::TextUnformatted(instr.bytes.c_str());
 
             ImGui::TableNextColumn();
-            ImGui::Text("%s", di.mnemonic.c_str());
+            ImGui::TextUnformatted(instr.mnemonic.c_str());
 
             ImGui::TableNextColumn();
-            ImGui::Text("%s", di.operands.c_str());
+            ImGui::TextUnformatted(instr.operands.c_str());
 
-            currentAddr += di.size;
-            if (currentAddr < di.size) break;  // Overflow
+            currentAddr += instr.size;
+            if (currentAddr < instr.size) {
+                break;  // Overflow
+            }
         }
         ImGui::EndTable();
     }
