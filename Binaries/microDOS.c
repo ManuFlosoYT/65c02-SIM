18f6e2d4375afebce1a98655fc2fbefd98790c1f
ManuFlosoYT <46373952+ManuFlosoYT@users.noreply.github.com>
2026-03-01 17:05:28 +0100

    Update microDOS.c

--- Binaries/microDOS.c
+++ Binaries/microDOS.c
@@ -11,22 +11,125 @@ char* args[MAX_ARGS];
 int arg_count;
 
 void tokenize(char* buf) {
-    char* p;
-    p = buf;
+    char* p = buf;
     arg_count = 0;
 
     while (*p && arg_count < MAX_ARGS) {
-        /* Skip spaces */
         while (*p == ' ') p++;
         if (*p == '\0') break;
 
         args[arg_count++] = p;
 
-        /* Find end of token */
         while (*p && *p != ' ') p++;
         if (*p == ' ') {
             *p = '\0';
             p++;
+        }
+    }
+}
+
+void auto_complete(char* buf, int* len, int max_len) {
+    int last_space = -1;
+    int i;
+    char* token;
+    int token_len;
+    char match[64];
+    char* file_prefix;
+    int match_len;
+    int pref_len;
+    static const char* commands[] = {"ls", "cat", "touch", "mkdir", "cd", "rm", "nano", "help", "exit", NULL};
+
+    for (i = 0; i < *len; i++) {
+        if (buf[i] == ' ') last_space = i;
+    }
+
+    token = &buf[last_space + 1];
+    token_len = *len - (last_space + 1);
+    file_prefix = token;
+
+    if (token_len == 0) return;
+
+    match[0] = '\0';
+
+    if (last_space == -1) {
+        for (i = 0; commands[i] != NULL; i++) {
+            if (strncmp(commands[i], token, token_len) == 0) {
+                strcpy(match, commands[i]);
+                break;
+            }
+        }
+    } else {
+        char search_path[64];
+        int prefix_len;
+        int last_slash = -1;
+        int path_len;
+        SD_DIR dir;
+        SD_INFO fno;
+
+        strcpy(search_path, ".");
+        prefix_len = token_len;
+        
+        for (i = 0; i < token_len; i++) {
+            if (token[i] == '/') last_slash = i;
+        }
+        
+        if (last_slash != -1) {
+            path_len = last_slash;
+            if (path_len > 0 && path_len < 64) {
+                strncpy(search_path, token, path_len);
+                search_path[path_len] = '\0';
+            } else if (path_len == 0) {
+                strcpy(search_path, "/");
+            }
+            file_prefix = &token[last_slash + 1];
+            prefix_len = token_len - (last_slash + 1);
+        }
+
+        if (sd_opendir(&dir, search_path)) {
+            while (sd_readdir(&dir, &fno)) {
+                if (strncmp(fno.fname, file_prefix, prefix_len) == 0) {
+                    strcpy(match, fno.fname);
+                    break;
+                }
+            }
+            sd_closedir(&dir);
+        }
+    }
+
+    if (match[0] != '\0') {
+        match_len = strlen(match);
+        pref_len = (last_space == -1) ? token_len : (strlen(token) - (file_prefix - token));
+        
+        for (i = pref_len; i < match_len && *len < max_len - 1; i++) {
+            buf[*len] = match[i];
+            (*len)++;
+            bios_putchar(match[i]);
+        }
+    }
+}
+
+void os_read_line(char* buf, int max_len) {
+    int len = 0;
+    char c;
+    while (1) {
+        c = bios_getchar();
+        if (c == '\r' || c == '\n') {
+            buf[len] = '\0';
+            bios_putchar('\r');
+            bios_putchar('\n');
+            break;
+        } else if (c == '\b' || c == 8 || c == 127) {
+            if (len > 0) {
+                len--;
+                bios_putchar(' ');
+                bios_putchar('\b');
+            }
+        } else if (c == '\t') {
+            auto_complete(buf, &len, max_len);
+        } else {
+            if (len < max_len - 1) {
+                buf[len++] = c;
+            }
         }
     }
 }
@@ -36,6 +139,8 @@ void cmd_ls() {
     SD_INFO fno;
     char* path;
     char size_str[11];
+    unsigned long sz;
+    int i;
 
     path = (arg_count > 1) ? args[1] : ".";
 
@@ -44,9 +149,8 @@ void cmd_ls() {
             if (fno.fattrib & AM_DIR) {
                 print_str("[DIR]      ");
             } else {
-                /* Simple number to string for size */
-                unsigned long sz = fno.fsize;
-                int i = 9;
+                sz = fno.fsize;
+                i = 9;
                 size_str[10] = '\0';
                 for (; i >= 0; i--) {
                     size_str[i] = (sz > 0 || i == 9) ? (char)((sz % 10) + '0') : ' ';
@@ -127,9 +231,8 @@ void cmd_rm() {
 void cmd_nano() {
     SD_FILE fp;
     char c;
-    char s[2];
-
-    s[1] = '\0';
+    static char editor_buf[4096];
+    int editor_len = 0;
 
     if (arg_count < 2) {
         println("Usage: nano <file>");
@@ -146,20 +249,30 @@ void cmd_nano() {
 
     while (1) {
         c = bios_getchar();
-        if (c == 27) break; /* ESC */
+        if (c == 27) break;
 
-        if (c == '\r' || c == '\n') {
-            bios_putchar('\r');
-            bios_putchar('\n');
-            sd_puts(&fp, "\n");
-        } else if (c == 8 || c == 127) {
-            /* Backspace ignored for simplicity */
+        if (c == '\b' || c == 8 || c == 127) {
+            if (editor_len > 0) {
+                editor_len--;
+                bios_putchar(' ');
+                bios_putchar('\b');
+            }
+        } else if (c == '\r' || c == '\n') {
+            if (editor_len < sizeof(editor_buf) - 1) {
+                editor_buf[editor_len++] = '\n';
+                bios_putchar('\r');
+                bios_putchar('\n');
+            }
         } else {
-            s[0] = c;
-            sd_puts(&fp, s);
+            if (editor_len < sizeof(editor_buf) - 1) {
+                editor_buf[editor_len++] = c;
+            }
         }
     }
 
+    editor_buf[editor_len] = '\0';
+    sd_puts(&fp, editor_buf);
+    
     sd_close(&fp);
     println("");
     println("--- SAVED ---");
@@ -169,7 +282,7 @@ int main(void) {
     char cwd[64];
     INIT_BUFFER();
 
-    println("microDOS v1.0");
+    println("microDOS v1.1");
     println("Mounting SD...");
 
     if (!sd_mount()) {
@@ -187,7 +300,7 @@ int main(void) {
         print_str(cwd);
         print_str(" > ");
 
-        read_line(line, MAX_LINE);
+        os_read_line(line, MAX_LINE);
         tokenize(line);
 
         if (arg_count == 0) continue;
@@ -207,7 +320,7 @@ int main(void) {
         } else if (strcmp(args[0], "nano") == 0) {
             cmd_nano();
         } else if (strcmp(args[0], "help") == 0) {
-            println("Commands: ls, cd, mkdir, touch, cat, rm, nano, help");
+            println("Commands: ls, cd, mkdir, touch, cat, rm, nano, help, exit");
         } else if (strcmp(args[0], "exit") == 0) {
             break;
         } else {
