d46d6a2b9b3dc9a5707f689d934f552e8a346256
ManuFlosoYT <46373952+ManuFlosoYT@users.noreply.github.com>
2026-04-05 18:19:40 +0200

    Update mingw-toolchain.cmake

--- cmake/mingw-toolchain.cmake
+++ cmake/mingw-toolchain.cmake
@@ -1,4 +1,5 @@
 set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_VERSION 10.0)
 set(CMAKE_C_COMPILER x86_64-w64-mingw32ucrt-gcc)
 set(CMAKE_CXX_COMPILER x86_64-w64-mingw32ucrt-g++)
 set(CMAKE_RC_COMPILER x86_64-w64-mingw32ucrt-windres)
