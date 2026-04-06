b6e09027174ea15b3657228824a581c51df14394
ManuFlosoYT <46373952+ManuFlosoYT@users.noreply.github.com>
2026-04-06 19:26:13 +0200

    Update BCS.cpp

--- Hardware/CPU/Instructions/BCS.cpp
+++ Hardware/CPU/Instructions/BCS.cpp
@@ -9,6 +9,8 @@ template <bool Debug>
 void BCS::Execute(CPU& cpu, Bus& bus) {
     int8_t dir = cpu.FetchByte<Debug>(bus);
     if(cpu.C == 1) {
+        cpu.remainingCycles++;
+        cpu.AddPageCrossPenalty(cpu.PC, static_cast<Word>(cpu.PC + dir));
         cpu.PC += dir;
         cpu.UpdatePagePtr(bus);
     }
