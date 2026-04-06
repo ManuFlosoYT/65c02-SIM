edc4f1b6cdd4f45076eb76e82a76fd07f4025712
ManuFlosoYT <46373952+ManuFlosoYT@users.noreply.github.com>
2026-04-06 23:16:45 +0200

    Update CartridgeLoader.h

--- Hardware/Core/CartridgeLoader.h
+++ Hardware/Core/CartridgeLoader.h
@@ -12,10 +12,11 @@ public:
     static bool LoadFromMemory(const uint8_t* data, size_t size, Cartridge& outCartridge, std::string& errorMsg);
     static bool SaveSDToZip(const Cartridge& cart);
 
-private:
     static void ParseMetadata(const nlohmann::json& manifestJson, Cartridge& outCartridge);
     static void ParseConfig(const nlohmann::json& manifestJson, Cartridge& outCartridge);
     static void ParseBus(const nlohmann::json& manifestJson, Cartridge& outCartridge);
+
+private:
     static bool ReadRomData(void* zip_archive, const std::string& romFileName, Cartridge& outCartridge, std::string& errorMsg);
     static bool ReadVramData(void* zip_archive, const std::string& vramFileName, Cartridge& outCartridge, std::string& errorMsg);
     static bool LoadInternal(void* zip_archive, Cartridge& outCartridge, std::string& errorMsg);
