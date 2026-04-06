3699a0dc648cb92b5cfae5d75a86a2a08b253c06
ManuFlosoYT <46373952+ManuFlosoYT@users.noreply.github.com>
2026-04-06 19:26:12 +0200

    Update BEQ.cpp

--- Hardware/CPU/Instructions/BEQ.cpp
+++ Hardware/CPU/Instructions/BEQ.cpp
@@ -9,6 +9,8 @@ template <bool Debug>
 void BEQ::Execute(CPU& cpu, Bus& bus) {
     int8_t dir = cpu.FetchByte<Debug>(bus);
     if(cpu.Z == 1) {
+        cpu.remainingCycles++;
+        cpu.AddPageCrossPenalty(cpu.PC, static_cast<Word>(cpu.PC + dir));
         cpu.PC += dir;
         cpu.UpdatePagePtr(bus);
     }
