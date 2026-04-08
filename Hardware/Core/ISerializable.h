c3cea16ae4e76492ac330391d4a4f13e4a7bb17f
ManuFlosoYT <46373952+ManuFlosoYT@users.noreply.github.com>
2026-04-08 18:29:42 +0200

    Update ISerializable.h

--- Hardware/Core/ISerializable.h
+++ Hardware/Core/ISerializable.h
@@ -1,8 +1,9 @@
 #pragma once
+#include <cstdint>
 #include <iostream>
-#include <cstring>
 #include <type_traits>
-#include <array>
+#include <vector>
+#include <string>
 
 namespace Hardware {
 
@@ -21,17 +22,38 @@ class ISerializable {
     template <typename T>
     static void Serialize(std::ostream& out, const T& value) {
         static_assert(std::is_trivially_copyable_v<T>, "Only trivially copyable types can be serialized using this helper");
-        std::array<char, sizeof(T)> buffer{};
-        std::memcpy(buffer.data(), &value, sizeof(T));
-        out.write(buffer.data(), sizeof(T));
+        out.write(reinterpret_cast<const char*>(&value), sizeof(T));
+    }
+
+    template <typename T>
+    static void Serialize(std::ostream& out, const std::vector<T>& vec) {
+        static_assert(std::is_trivially_copyable_v<T>, "Only vectors of trivially copyable types can be serialized using this helper");
+        out.write(reinterpret_cast<const char*>(vec.data()), static_cast<std::streamsize>(vec.size() * sizeof(T)));
     }
 
     template <typename T>
     static void Deserialize(std::istream& inputStream, T& value) {
         static_assert(std::is_trivially_copyable_v<T>, "Only trivially copyable types can be deserialized using this helper");
-        std::array<char, sizeof(T)> buffer{};
-        inputStream.read(buffer.data(), sizeof(T));
-        std::memcpy(&value, buffer.data(), sizeof(T));
+        inputStream.read(reinterpret_cast<char*>(&value), sizeof(T));
+    }
+
+    template <typename T>
+    static void Deserialize(std::istream& inputStream, std::vector<T>& vec) {
+        static_assert(std::is_trivially_copyable_v<T>, "Only vectors of trivially copyable types can be deserialized using this helper");
+        inputStream.read(reinterpret_cast<char*>(vec.data()), static_cast<std::streamsize>(vec.size() * sizeof(T)));
+    }
+
+    static void Serialize(std::ostream& out, const std::string& str) {
+        auto len = static_cast<uint32_t>(str.length());
+        Serialize(out, len);
+        out.write(str.c_str(), static_cast<std::streamsize>(len));
+    }
+
+    static void Deserialize(std::istream& inputStream, std::string& str) {
+        uint32_t len = 0;
+        Deserialize(inputStream, len);
+        str.assign(len, '\0');
+        inputStream.read(str.data(), static_cast<std::streamsize>(len));
     }
 };
 
