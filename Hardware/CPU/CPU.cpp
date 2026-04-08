3c4d55355586c6998b7bab5668e64ba0d2037177
ManuFlosoYT <46373952+ManuFlosoYT@users.noreply.github.com>
2026-04-08 17:32:40 +0200

    Update CPU.cpp

--- Hardware/CPU/CPU.cpp
+++ Hardware/CPU/CPU.cpp
@@ -3,40 +3,34 @@
 namespace Hardware {
 
 bool CPU::SaveState(std::ostream& out) const {
-    out.write(reinterpret_cast<const char*>(&PC), sizeof(PC));
-    out.write(reinterpret_cast<const char*>(&SP), sizeof(SP));
-    out.write(reinterpret_cast<const char*>(&A), sizeof(A));
-    out.write(reinterpret_cast<const char*>(&X), sizeof(X));
-    out.write(reinterpret_cast<const char*>(&Y), sizeof(Y));
-
-    Byte status = GetStatus();
-    out.write(reinterpret_cast<const char*>(&status), sizeof(status));
-
-    out.write(reinterpret_cast<const char*>(&isInit), sizeof(isInit));
-    out.write(reinterpret_cast<const char*>(&waiting), sizeof(waiting));
-    out.write(reinterpret_cast<const char*>(&remainingCycles),
-              sizeof(remainingCycles));
-    out.write(reinterpret_cast<const char*>(&cycleAccurate),
-              sizeof(cycleAccurate));
-
+    Serialize(out, PC);
+    Serialize(out, SP);
+    Serialize(out, A);
+    Serialize(out, X);
+    Serialize(out, Y);
+    Serialize(out, GetStatus());
+    Serialize(out, isInit);
+    Serialize(out, waiting);
+    Serialize(out, remainingCycles);
+    Serialize(out, cycleAccurate);
     return out.good();
 }
 
 bool CPU::LoadState(std::istream& inputStream) {
-    inputStream.read(reinterpret_cast<char*>(&PC), sizeof(PC));
-    inputStream.read(reinterpret_cast<char*>(&SP), sizeof(SP));
-    inputStream.read(reinterpret_cast<char*>(&A), sizeof(A));
-    inputStream.read(reinterpret_cast<char*>(&X), sizeof(X));
-    inputStream.read(reinterpret_cast<char*>(&Y), sizeof(Y));
+    Deserialize(inputStream, PC);
+    Deserialize(inputStream, SP);
+    Deserialize(inputStream, A);
+    Deserialize(inputStream, X);
+    Deserialize(inputStream, Y);
 
     Byte status = 0;
-    inputStream.read(reinterpret_cast<char*>(&status), sizeof(status));
+    Deserialize(inputStream, status);
     SetStatus(status);
 
-    inputStream.read(reinterpret_cast<char*>(&isInit), sizeof(isInit));
-    inputStream.read(reinterpret_cast<char*>(&waiting), sizeof(waiting));
-    inputStream.read(reinterpret_cast<char*>(&remainingCycles), sizeof(remainingCycles));
-    inputStream.read(reinterpret_cast<char*>(&cycleAccurate), sizeof(cycleAccurate));
+    Deserialize(inputStream, isInit);
+    Deserialize(inputStream, waiting);
+    Deserialize(inputStream, remainingCycles);
+    Deserialize(inputStream, cycleAccurate);
 
     return inputStream.good();
 }
