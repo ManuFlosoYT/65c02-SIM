391a0a4c685015d587ff936bf03028a3404bedf7
ManuFlosoYT <46373952+ManuFlosoYT@users.noreply.github.com>
2026-04-08 17:14:04 +0200

    Update CPU.cpp

--- Hardware/CPU/CPU.cpp
+++ Hardware/CPU/CPU.cpp
@@ -3,40 +3,40 @@
 namespace Hardware {
 
 bool CPU::SaveState(std::ostream& out) const {
-    out.write(reinterpret_cast<const char*>(&PC), sizeof(PC));  // NOLINT
-    out.write(reinterpret_cast<const char*>(&SP), sizeof(SP));  // NOLINT
-    out.write(reinterpret_cast<const char*>(&A), sizeof(A));    // NOLINT
-    out.write(reinterpret_cast<const char*>(&X), sizeof(X));    // NOLINT
-    out.write(reinterpret_cast<const char*>(&Y), sizeof(Y));    // NOLINT
+    out.write(reinterpret_cast<const char*>(&PC), sizeof(PC));
+    out.write(reinterpret_cast<const char*>(&SP), sizeof(SP));
+    out.write(reinterpret_cast<const char*>(&A), sizeof(A));
+    out.write(reinterpret_cast<const char*>(&X), sizeof(X));
+    out.write(reinterpret_cast<const char*>(&Y), sizeof(Y));
 
     Byte status = GetStatus();
-    out.write(reinterpret_cast<const char*>(&status), sizeof(status));  // NOLINT
+    out.write(reinterpret_cast<const char*>(&status), sizeof(status));
 
-    out.write(reinterpret_cast<const char*>(&isInit), sizeof(isInit));    // NOLINT
-    out.write(reinterpret_cast<const char*>(&waiting), sizeof(waiting));  // NOLINT
-    out.write(reinterpret_cast<const char*>(&remainingCycles),            // NOLINT
+    out.write(reinterpret_cast<const char*>(&isInit), sizeof(isInit));
+    out.write(reinterpret_cast<const char*>(&waiting), sizeof(waiting));
+    out.write(reinterpret_cast<const char*>(&remainingCycles),
               sizeof(remainingCycles));
-    out.write(reinterpret_cast<const char*>(&cycleAccurate),  // NOLINT
+    out.write(reinterpret_cast<const char*>(&cycleAccurate),
               sizeof(cycleAccurate));
 
     return out.good();
 }
 
 bool CPU::LoadState(std::istream& inputStream) {
-    inputStream.read(reinterpret_cast<char*>(&PC), sizeof(PC));  // NOLINT
-    inputStream.read(reinterpret_cast<char*>(&SP), sizeof(SP));  // NOLINT
-    inputStream.read(reinterpret_cast<char*>(&A), sizeof(A));    // NOLINT
-    inputStream.read(reinterpret_cast<char*>(&X), sizeof(X));    // NOLINT
-    inputStream.read(reinterpret_cast<char*>(&Y), sizeof(Y));    // NOLINT
+    inputStream.read(reinterpret_cast<char*>(&PC), sizeof(PC));
+    inputStream.read(reinterpret_cast<char*>(&SP), sizeof(SP));
+    inputStream.read(reinterpret_cast<char*>(&A), sizeof(A));
+    inputStream.read(reinterpret_cast<char*>(&X), sizeof(X));
+    inputStream.read(reinterpret_cast<char*>(&Y), sizeof(Y));
 
     Byte status = 0;
-    inputStream.read(reinterpret_cast<char*>(&status), sizeof(status));  // NOLINT
+    inputStream.read(reinterpret_cast<char*>(&status), sizeof(status));
     SetStatus(status);
 
-    inputStream.read(reinterpret_cast<char*>(&isInit), sizeof(isInit));                    // NOLINT
-    inputStream.read(reinterpret_cast<char*>(&waiting), sizeof(waiting));                  // NOLINT
-    inputStream.read(reinterpret_cast<char*>(&remainingCycles), sizeof(remainingCycles));  // NOLINT
-    inputStream.read(reinterpret_cast<char*>(&cycleAccurate), sizeof(cycleAccurate));      // NOLINT
+    inputStream.read(reinterpret_cast<char*>(&isInit), sizeof(isInit));
+    inputStream.read(reinterpret_cast<char*>(&waiting), sizeof(waiting));
+    inputStream.read(reinterpret_cast<char*>(&remainingCycles), sizeof(remainingCycles));
+    inputStream.read(reinterpret_cast<char*>(&cycleAccurate), sizeof(cycleAccurate));
 
     return inputStream.good();
 }
