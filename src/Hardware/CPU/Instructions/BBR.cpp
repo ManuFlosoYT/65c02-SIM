68f5db746466399e3f1121deb78467ad4579c87a
ManuFlosoYT <46373952+ManuFlosoYT@users.noreply.github.com>
2026-04-10 13:54:09 +0200

    Update BBR.cpp

--- src/Hardware/CPU/Instructions/BBR.cpp
+++ src/Hardware/CPU/Instructions/BBR.cpp
@@ -5,108 +5,59 @@
 
 namespace Hardware::Instructions {
 
-template <bool Debug>
-void BBR::Execute0(CPU& cpu, Bus& bus) {
+template <bool Debug, Byte Mask>
+static void ExecuteBBR(CPU& cpu, Bus& bus) {
     Byte zpAddr = cpu.FetchByte<Debug>(bus);
     int8_t offset = cpu.FetchByte<Debug>(bus);
-    Word dir = cpu.PC + offset;
+    Word target = static_cast<Word>(cpu.PC + offset);
     Byte val = cpu.ReadByte<Debug>(zpAddr, bus);
 
-    if ((val & 0b00000001) == 0) {
-        cpu.PC = dir;
+    if ((val & Mask) == 0) {
+        cpu.remainingCycles++;
+        cpu.AddPageCrossPenalty(cpu.PC, target);
+        cpu.PC = target;
         cpu.UpdatePagePtr(bus);
     }
+}
+
+template <bool Debug>
+void BBR::Execute0(CPU& cpu, Bus& bus) {
+    ExecuteBBR<Debug, 0b00000001>(cpu, bus);
 }
 
 template <bool Debug>
 void BBR::Execute1(CPU& cpu, Bus& bus) {
-    Byte zpAddr = cpu.FetchByte<Debug>(bus);
-    int8_t offset = cpu.FetchByte<Debug>(bus);
-    Word dir = cpu.PC + offset;
-    Byte val = cpu.ReadByte<Debug>(zpAddr, bus);
-
-    if ((val & 0b00000010) == 0) {
-        cpu.PC = dir;
-        cpu.UpdatePagePtr(bus);
-    }
+    ExecuteBBR<Debug, 0b00000010>(cpu, bus);
 }
 
 template <bool Debug>
 void BBR::Execute2(CPU& cpu, Bus& bus) {
-    Byte zpAddr = cpu.FetchByte<Debug>(bus);
-    int8_t offset = cpu.FetchByte<Debug>(bus);
-    Word dir = cpu.PC + offset;
-    Byte val = cpu.ReadByte<Debug>(zpAddr, bus);
-
-    if ((val & 0b00000100) == 0) {
-        cpu.PC = dir;
-        cpu.UpdatePagePtr(bus);
-    }
+    ExecuteBBR<Debug, 0b00000100>(cpu, bus);
 }
 
 template <bool Debug>
 void BBR::Execute3(CPU& cpu, Bus& bus) {
-    Byte zpAddr = cpu.FetchByte<Debug>(bus);
-    int8_t offset = cpu.FetchByte<Debug>(bus);
-    Word dir = cpu.PC + offset;
-    Byte val = cpu.ReadByte<Debug>(zpAddr, bus);
-
-    if ((val & 0b00001000) == 0) {
-        cpu.PC = dir;
-        cpu.UpdatePagePtr(bus);
-    }
+    ExecuteBBR<Debug, 0b00001000>(cpu, bus);
 }
 
 template <bool Debug>
 void BBR::Execute4(CPU& cpu, Bus& bus) {
-    Byte zpAddr = cpu.FetchByte<Debug>(bus);
-    int8_t offset = cpu.FetchByte<Debug>(bus);
-    Word dir = cpu.PC + offset;
-    Byte val = cpu.ReadByte<Debug>(zpAddr, bus);
-
-    if ((val & 0b00010000) == 0) {
-        cpu.PC = dir;
-        cpu.UpdatePagePtr(bus);
-    }
+    ExecuteBBR<Debug, 0b00010000>(cpu, bus);
 }
 
 template <bool Debug>
 void BBR::Execute5(CPU& cpu, Bus& bus) {
-    Byte zpAddr = cpu.FetchByte<Debug>(bus);
-    int8_t offset = cpu.FetchByte<Debug>(bus);
-    Word dir = cpu.PC + offset;
-    Byte val = cpu.ReadByte<Debug>(zpAddr, bus);
-
-    if ((val & 0b00100000) == 0) {
-        cpu.PC = dir;
-        cpu.UpdatePagePtr(bus);
-    }
+    ExecuteBBR<Debug, 0b00100000>(cpu, bus);
 }
 
 template <bool Debug>
 void BBR::Execute6(CPU& cpu, Bus& bus) {
-    Byte zpAddr = cpu.FetchByte<Debug>(bus);
-    int8_t offset = cpu.FetchByte<Debug>(bus);
-    Word dir = cpu.PC + offset;
-    Byte val = cpu.ReadByte<Debug>(zpAddr, bus);
-
-    if ((val & 0b01000000) == 0) {
-        cpu.PC = dir;
-        cpu.UpdatePagePtr(bus);
-    }
+    ExecuteBBR<Debug, 0b01000000>(cpu, bus);
 }
 
 template <bool Debug>
 void BBR::Execute7(CPU& cpu, Bus& bus) {
-    Byte zpAddr = cpu.FetchByte<Debug>(bus);
-    int8_t offset = cpu.FetchByte<Debug>(bus);
-    Word dir = cpu.PC + offset;
-    Byte val = cpu.ReadByte<Debug>(zpAddr, bus);
-
-    if ((val & 0b10000000) == 0) {
-        cpu.PC = dir;
-        cpu.UpdatePagePtr(bus);
-    }
+    ExecuteBBR<Debug, 0b10000000>(cpu, bus);
 }
 
 template void BBR::Execute0<true>(CPU&, Bus&);
