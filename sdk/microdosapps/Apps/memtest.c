66cb07211669e49270051d04c046f6c42a1e75f8
ManuFlosoYT <46373952+ManuFlosoYT@users.noreply.github.com>
2026-09-17 10:37:41 +0200

    Update memtest.c

--- sdk/microdosapps/Apps/memtest.c
+++ sdk/microdosapps/Apps/memtest.c
@@ -6,13 +6,6 @@
 #include <stdint.h>
 #include "Libs/app-bios.h"
 
-
-
-#define VIA_T1C_L  (*(volatile uint8_t*)0x6004)
-#define VIA_T1C_H  (*(volatile uint8_t*)0x6005)
-#define VIA_ACR    (*(volatile uint8_t*)0x600B)
-#define VIA_IFR    (*(volatile uint8_t*)0x600D)
-
 // Global to prevent compiler optimization on read loops
 volatile uint8_t benchmark_sink;
 
