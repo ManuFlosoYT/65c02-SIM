36f51869d6a8c782351a9fa119cc6a4fc6e7ce58
ManuFlosoYT <46373952+ManuFlosoYT@users.noreply.github.com>
2026-09-24 20:06:03 +0200

    Create cp.c

---
+++ sdk/microdosapps/Apps/cp.c
@@ -0,0 +1,42 @@
+/* cp.c - microDOS command */
+#include "../Libs/app-bios.h"
+#include <stdint.h>
+#include <string.h>
+
+int main(void) {
+    SD_FILE fp_src;
+    SD_FILE fp_dst;
+    char **args = _args_ptr;
+    uint8_t buf[512];
+    int16_t bytes_read;
+    int16_t bytes_written;
+
+    if (arg_count < 3) {
+        println("Usage: cp <src> <dst>");
+        return 1;
+    }
+
+    if (!sd_open(&fp_src, args[1], SD_READ)) {
+        println("Error: Source not found.");
+        return 1;
+    }
+
+    if (!sd_open(&fp_dst, args[2], SD_WRITE | SD_CREATE_ALWAYS)) {
+        sd_close(&fp_src);
+        println("Error: Cannot create dst.");
+        return 1;
+    }
+
+    while ((bytes_read = sd_read(&fp_src, buf, sizeof(buf))) > 0) {
+        bytes_written = sd_write(&fp_dst, buf, bytes_read);
+        if (bytes_written != bytes_read) {
+            println("Error: Write failed.");
+            break;
+        }
+    }
+
+    sd_close(&fp_dst);
+    sd_close(&fp_src);
+
+    return 0;
+}
